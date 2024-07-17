fee025a6ea7e267e658c696f6e5985912e55900a
Programação em Python 2024.4 <36129532024.4@senacrjedu.local>
2024-07-17 12:01:49 -0300

    Primeiro commit

---
+++ if.py
@@ -0,0 +1,17 @@
+# Verificar se é maior de idade
+
+maiorIdade = 18
+idade = int(input('Digite sua idade\n'))
+
+if (idade >= maiorIdade):
+    print('Eh maior de idade')
+else:
+    print('Eh menor de idade')
+
+# if (variavel 'Sinal' variavel)
+# é igual/diferente?
+# Igual: ==
+# Diferente: !=
+# é maior/menor?
+# Maior: > (Maior ou igual: >=)
+# Menor: < (Menor ou igual: <=)
