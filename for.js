f0ab89325ce38c5d70dc7685974394ee87c84d00
Programação em Python 2024.4 <36129532024.4@senacrjedu.local>
2024-07-19 12:00:02 -0300

    Hey

---
+++ for.js
@@ -0,0 +1,10 @@
+// usuário digitará um número e 
+// completará o número digitado até cem, 
+// apenas com números pares.
+usuario = 30
+for(let i=usuario;i<=100;i++){
+
+    if(i%2==0)
+        console.log(i)
+}
+
---
+++ while.js
@@ -0,0 +1,28 @@
+let i = 0
+var clicou = 1
+// while(true){
+//     if(clicou == 1){
+//         console.log('Saindo do loop')
+//         break
+//     }
+// }
+console.log('While')
+while(i<=10){
+    console.log(i)
+    i++
+    // i+=1 (i=i+1)
+}
+// i=0
+console.log('Do While')
+do{
+    console.log(i)
+    i++
+}while(i<=10)
+
+// i = 10
+
+// while(i>=0){
+//     console.log(i)
+//     i--
+//     // i-=1 (i=i-1)
+// }
